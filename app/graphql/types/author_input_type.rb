83ee42472638d15a463cd391875eed0bc7ffe630
Nikolay Kalashnikov <kalash-job@yandex.ru>
2023-09-21 18:23:10 +0200

    add field update_author

--- app/graphql/types/author_input_type.rb
+++ app/graphql/types/author_input_type.rb
@@ -2,11 +2,11 @@
 
 class Types::AuthorInputType < GraphQL::Schema::InputObject
   graphql_name 'AuthorInputType'
-  description 'All the attributes for creating an author'
+  description 'All the attributes for creating/updating an author'
 
+  argument :id, ID, required: false
   argument :first_name, String, required: true
   argument :last_name, String, required: true
   argument :yob, Integer, required: true
   argument :is_alive, Boolean, required: false
 end
-
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,14 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
+
+    field :update_author, Boolean, null: false, description: 'Update an author' do
+      argument :author, Types::AuthorInputType, required: true
+    end
+
+    def update_author(author:)
+      existing_author = Author.find_by(id: author[:id])
+      existing_author.present? && existing_author.update(author.to_h.except(:id))
+    end
   end
 end
